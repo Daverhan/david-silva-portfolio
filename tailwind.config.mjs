a6fd5d932c4b22d0b0852048c4b5151a4d514cf8
David Silva <daverhan02@gmail.com>
2024-05-05 10:50:35 -0400

    Implement hero component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        hero_background: "url('/src/assets/hero-bg.avif')",
+      },
+    },
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
